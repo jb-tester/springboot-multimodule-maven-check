2db0a1c1355a37c32bdb0985db9a9466d75eb865
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2025-03-20 19:39:24 +0100

    comments

--- test-module/src/main/java/com/mytests/spring/multiModuleCheck/TestService.java
+++ test-module/src/main/java/com/mytests/spring/multiModuleCheck/TestService.java
@@ -1,6 +1,5 @@
 package com.mytests.spring.multiModuleCheck;
 
-import com.mytests.spring.multiModuleCheck.useBeans.UserComponent;
 import org.springframework.stereotype.Service;
 
 
@@ -10,7 +9,7 @@ class TestService {
 
     private final AppService appService;
 
-    // navigates to Appservice, but there is no back navigation
+    // navigates to AppService bean, but there is no back navigation
     TestService(AppService appService) {
         this.appService = appService;
     }
--- use-impl-module/src/main/java/com/mytests/spring/multiModuleCheck/useBeans/UserComponent.java
+++ use-impl-module/src/main/java/com/mytests/spring/multiModuleCheck/useBeans/UserComponent.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+// navigation to the autowired beans and back works
 @Component
 public class UserComponent {
 
