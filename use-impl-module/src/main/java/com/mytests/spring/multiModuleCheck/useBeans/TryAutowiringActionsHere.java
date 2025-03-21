918b3d13f82d403a5c6cafc7e775ddf7086959a2
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2025-03-21 14:53:51 +0100

    YT links

--- use-impl-module/src/main/java/com/mytests/spring/multiModuleCheck/useBeans/TryAutowiringActionsHere.java
+++ use-impl-module/src/main/java/com/mytests/spring/multiModuleCheck/useBeans/TryAutowiringActionsHere.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TryAutowiringActionsHere {
-
+     // https://youtrack.jetbrains.com/issue/IDEA-369588/Spring-AutowireBean...-works-incorrectly-in-the-multimodule-projects
     // invoke Generate | Autowire Beans... action here
     // Expected:
     // the beans defined in the `impl-module`, i.e.
@@ -16,16 +16,19 @@ public class TryAutowiringActionsHere {
     // for `MyBeanClass` type nothing is shown
     // No autowiring errors are shown for the manually injected beans
     // (see `UserComponent` class)
-    // injection by completion works to some extent:
+
+    // https://youtrack.jetbrains.com/issue/IDEA-369590/Spring-autowiring-by-completion-works-incorrectly-in-the-multimodule-projects
+    // injection by completion works to some extent only:
     // you can complete the beans by the supertype entering,
     // but not by the concrete bean name
 
     public void checkInjectionByCompletionHere() {
         System.out.println("====== injection by completion =====");
+        // Check autowiring by completion here:
         // myFooSer  // doesn't work
         // myBean1   // doesn't work
-        // myBeanC // works
-        // myServ   // works
+        // myBeanC // works - supertype
+        // myServ   // works - supertype
         System.out.println("===================");
     }
 
